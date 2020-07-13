9886c05883b097bfa9564ecfcb5597d0c8fb933f
Nick Silva <silva.d.nicholas@gmail.com>
2020-07-13 16:26:26 -0500

    implemented zhang-suen

--- test.py
+++ test.py
@@ -39,6 +39,51 @@ def skeletonize(image):
     return skeleton
 
 
+def neighbors(x, y, img):
+    l, r, t, b = x-1, x+1, y-1, y+1
+    return [img[t, x], img[t, r], img[y, r], img[b, r], img[b, x], img[b, l], img[y][l], img[t][l]]
+
+
+def has_transitions(neighbors):
+    n = neighbors + neighbors[0:1]
+    tran = sum((a, b) == (255, 0) for a, b in zip(n, n[1:]))
+
+    return tran == 1
+
+
+def zhang_suen(img):
+    (h, w) = img.shape
+    remove = remove2 = [100]
+    copy = img.copy()
+    while remove or remove2:
+        remove = []
+        for y in range(1, h-1):
+            for x in range(1, w-1):
+                if copy[y][x] == 0:
+                    n = neighbors(x, y, copy)
+                    if (2 <= sum(n)//255 <= 6 and
+                        has_transitions(n) and
+                        n[2]*n[4]*n[6] == 0 and
+                            n[0]*n[2]*n[4] == 0):
+                        remove.append((x, y))
+        for x, y in remove:
+            copy.itemset((y, x), 255)
+
+        remove2 = []
+        for y in range(1, h-1):
+            for x in range(1, w-1):
+                if copy[y][x] == 0:
+                    n = neighbors(x, y, copy)
+                    if (2 <= sum(n)//255 <= 6 and
+                        has_transitions(n) and
+                        n[0]*n[4]*n[6] == 0 and
+                            n[0]*n[2]*n[6] == 0):
+                        remove2.append((x, y))
+        for x, y in remove2:
+            copy.itemset((y, x), 255)
+    return copy
+
+
 def custom_threshold(image, cutoff):
     (h, w) = image.shape
     for x in range(h):
@@ -57,22 +102,25 @@ def cv2_threshold(image):
 
 def process_image(image):
     maze = image.copy()
-    (h, w, d) = maze.shape
-    # x = 120/w
-    # maze = cv2.resize(maze, (120, int(x * h)))
-    maze = cv2.cvtColor(maze, cv2.COLOR_BGR2GRAY)
-    threshold = custom_threshold(maze, 100)
+    (h, w) = maze.shape
 
-    # element = cv2.getStructuringElement(cv2.MORPH_CROSS, (3, 3))
-    # threshold = cv2.dilate(threshold, element)
+    if background_color(maze) == 0:
+        maze = cv2.bitwise_not(maze)
+
+    #threshold = custom_threshold(maze, 150)
+    t, threshold = cv2.threshold(maze, 225, 255, cv2.THRESH_BINARY)
 
     cv2.imshow("main", threshold)
     cv2.imwrite("thresh.png", threshold)
-    skeleton = skeletonize(threshold)
-    cv2.imshow("skel", skeleton)
-    cv2.imwrite("skel.png", skeleton)
+
+    skel = zhang_suen(threshold)
+    #skel = skeletonize(threshold)
+    cv2.imshow("skeletonized", skel)
+
     cv2.waitKey(0)
 
 
-image = cv2.imread("maze.png")
-process_image(image)
+img0 = cv2.imread('test.png', 0)
+img1 = cv2.imread('color_maze.png', 0)
+img2 = cv2.imread('tc_maze.png', 0)
+process_image(img0)
